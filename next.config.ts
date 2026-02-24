6a9f433dc653db4e3e67f744429992f62c6ca9b9
Nefeli-cloud510 <202420102181@bfsu.edu.cn>
2026-02-25 02:42:29 +0800

    fix: unify next config for static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  /* config options here */
+   output: "export",
+  basePath: "/Chinaquest",
+  trailingSlash: true,
+  reactStrictMode: false,
+    images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
