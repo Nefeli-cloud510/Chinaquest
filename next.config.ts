b7b757415b43ca2b9074832c40fa95eb9c67abb2
Nefeli-cloud510 <202420102181@bfsu.edu.cn>
2026-02-25 15:38:46 +0800

    chore: remove duplicate chinaquest folder

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
+  turbopack: { root: __dirname },
   ...(isProd
     ? {
         basePath: `/${repoName}`,
